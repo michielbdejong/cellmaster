ddf84fbc1ec78ec5a96bd6c178bbada9c89dca7c
Michiel de Jong <michiel@unhosted.org>
2014-07-07 15:56:20 +0000

    add Rackspace backup agent and Docker

--- installer/install.sh
+++ installer/install.sh
@@ -1,8 +1,12 @@
+# Rackspace backup agent
+sudo sh -c 'wget -q "http://agentrepo.drivesrvr.com/debian/agentrepo.key" -O- | apt-key add -'
+sudo sh -c 'echo "deb [arch=amd64] http://agentrepo.drivesrvr.com/debian/ serveragent main" > /etc/apt/sources.list.d/driveclient.list'
+
 # based on http://plusbryan.com/my-first-5-minutes-on-a-server-or-essential-security-for-linux-servers
 passwd -d root
 apt-get -y update
 apt-get -y upgrade
-apt-get -y install vim ufw fail2ban unattended-upgrades logwatch
+apt-get -y install vim ufw fail2ban unattended-upgrades logwatch driveclient docker
 useradd -G sudo deploy
 mkdir -p /home/deploy/.ssh
 chmod 700 /home/deploy/.ssh
@@ -17,3 +21,7 @@ mv 10periodic /etc/apt/apt.conf.d/
 mv 50unattended-upgrades /etc/apt/apt.conf.d/
 mv 00logwatch /etc/cron.daily/
 passwd deploy
+
+# Docker
+docker pull thirdpartypeople/resite
+docker run -d -v /data-michiel:/data -p 80:80 -p 443:443 thirdpartypeople/resite
