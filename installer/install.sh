144a4cd76b5dd7c0dadbde76d48b84bd03989762
Michiel de Jong <michiel@unhosted.org>
2014-07-07 15:39:19 +0000

    small corrections

--- installer/install.sh
+++ installer/install.sh
@@ -6,6 +6,7 @@ apt-get -y install vim ufw fail2ban unattended-upgrades logwatch
 useradd -G sudo deploy
 mkdir -p /home/deploy/.ssh
 chmod 700 /home/deploy/.ssh
+mv .ssh/authorized_keys /home/deploy/.ssh/
 chmod 400 /home/deploy/.ssh/authorized_keys
 chown deploy:deploy /home/deploy -R
 ufw allow 22
@@ -16,4 +17,3 @@ mv 10periodic /etc/apt/apt.conf.d/
 mv 50unattended-upgrades /etc/apt/apt.conf.d/
 mv 00logwatch /etc/cron.daily/
 passwd deploy
-mv .ssh/authorized_keys /home/deploy/.ssh/
--- new-jessie.sh
+++ new-jessie.sh
@@ -1,5 +1,6 @@
 echo Setting up new cell in the Third Party People network.
 echo Looking for a virgin Debian Jessy server at IP address $1.
 echo Please enter the root password for the new server:
-scp -r ./installer/* root@$1:
-ssh deploy@$1 sh ./install.sh
+scp -r ./installer/.ssh root@$1:/root/
+scp -r ./installer/* root@$1:/root/
+ssh root@$1 sh ./install.sh
